5a4e2f4b630dc748b9009ab4a327fe08c1aa8be3
David Hovemeyer <david.hovemeyer@gmail.com>
2015-07-12 15:53:45 -0400

    end playback automatically

--- FunWithSound/src/edu/ycp/cs/funwithsound/Player.java
+++ FunWithSound/src/edu/ycp/cs/funwithsound/Player.java
@@ -6,16 +6,25 @@ import io.github.daveho.gervill4beads.Midi;
 import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.ShortMessage;
 
 import net.beadsproject.beads.core.AudioContext;
+import net.beadsproject.beads.core.Bead;
 
 /**
  * Play a composition.
  */
 public class Player {
+	// Delay before starting playback,
+	// to avoid early audio buffer underruns.
+	private static final long START_DELAY_US = 500000L;
+	
+	// Shut down this many microseconds after the last note off message.
+	private static final long IDLE_WAIT_US = 1000000L;
+	
 	private Composition composition;
 	private AudioContext ac;
 	
@@ -41,6 +50,7 @@ public class Player {
 		Map<Instrument, GervillUGen> instrMap = new IdentityHashMap<Instrument, GervillUGen>();
 		
 		// Convert figures to MidiEvents and schedule them to be played
+		long lastNoteOffUs = 0L;
 		for (Figure f : composition) {
 			Instrument instrument = f.getInstrument();
 			GervillUGen gervill = instrMap.get(instrument);
@@ -57,15 +67,45 @@ public class Player {
 				Strike s = rhythm.get(i);
 				Chord c = melody.get(i);
 				for (Integer note : c) {
+					long onTime = START_DELAY_US + f.getStartUs() + s.getStartUs();
+					long offTime = onTime + s.getDurationUs();
 					ShortMessage noteOn = Midi.createShortMessage(ShortMessage.NOTE_ON, note, s.getVelocity());
-					gervill.getSynthRecv().send(noteOn, f.getStartUs() + s.getStartUs());
+					gervill.getSynthRecv().send(noteOn, onTime);
 					ShortMessage noteOff = Midi.createShortMessage(ShortMessage.NOTE_OFF, note, s.getVelocity());
-					gervill.getSynthRecv().send(noteOff, f.getStartUs() + s.getStartUs() + s.getDurationUs());
+					gervill.getSynthRecv().send(noteOff, offTime);
+					// Keep track of the time of the last note off event
+					if (offTime > lastNoteOffUs) {
+						lastNoteOffUs = offTime;
+					}
 				}
 			}
 		}
 		
+		final long idleTimeUs = lastNoteOffUs + IDLE_WAIT_US;
+
+		// Register a shutdown hook to detect when playback is finished
+		final CountDownLatch latch = new CountDownLatch(1); 
+		ac.invokeAfterEveryFrame(new Bead() {
+			@Override
+			protected void messageReceived(Bead message) {
+				long timestampUs = ((long)ac.getTime()) * 1000L;
+				if (timestampUs >= idleTimeUs) {
+					// Notify main thread that playback is complete
+					latch.countDown();
+				}
+			}
+		});
+		
 		// Start the AudioContext!
 		ac.start();
+		
+		// Wait for playback to complete, then stop the AudioContext
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			System.out.println("Interrupted waiting for playback to complete");
+		}
+		ac.stop();
+		System.out.println("Playback finished");
 	}
 }
