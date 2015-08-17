897842890fecb49c0fd9c19fba211a451ddf63a2
David Hovemeyer <david.hovemeyer@gmail.com>
2015-08-17 08:30:30 -0400

    rename tr808

--- FunWithSound/demo/io/github/daveho/funwithsound/demo/Demo2.java
+++ FunWithSound/demo/io/github/daveho/funwithsound/demo/Demo2.java
@@ -16,16 +16,16 @@ public class Demo2 extends DemoBase {
 		setScale(Scale.melodicMinor(65));
 		setTempo(new Tempo(200, 8));
 
-		Instrument tr808 = percussion(M1);
-		Instrument tr808_kicks = percussion(M1);
+		Instrument drums = percussion(M1);
+		Instrument drums_kicks = percussion(M1);
 		Instrument bass = instr(ARACHNO, 38);
 		Instrument lead = instr(ARACHNO, 88);
 		
 		Rhythm kicks = r(p(0, 120), p(4));
-		Figure kicksf = pf(kicks, 35, tr808_kicks);
+		Figure kicksf = pf(kicks, 35, drums_kicks);
 		
 		Rhythm basichihatr = rr(p(0, 88), 1, 8);
-		Figure basichihatf = pf(basichihatr, 42, tr808);
+		Figure basichihatf = pf(basichihatr, 42, drums);
 		
 		Rhythm bassr = r(
 				s(0.000,2.365,79), s(2.1,0.709,101), s(3.5,0.7,110), s(4.45,0.184,118), s(4.95,0.375,110), s(5.95,1.040,110), s(6.95,0.873,106));
