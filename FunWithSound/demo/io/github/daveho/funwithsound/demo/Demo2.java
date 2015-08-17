979d2cd5608a2b396a6b55429b40c7db58124ad3
David Hovemeyer <david.hovemeyer@gmail.com>
2015-08-17 10:57:44 -0400

    added a melody part

--- FunWithSound/demo/io/github/daveho/funwithsound/demo/Demo2.java
+++ FunWithSound/demo/io/github/daveho/funwithsound/demo/Demo2.java
@@ -18,6 +18,7 @@ public class Demo2 extends DemoBase {
 		Instrument drums_kicks = percussion(M1);
 		Instrument bass = instr(ARACHNO, 38);
 		Instrument newage = instr(ARACHNO, 88);
+		Instrument lead = instr(ARACHNO, 11);
 		
 		Rhythm kicks = r(p(0, 120), p(4));
 		Figure kicksf = pf(kicks, 35, drums_kicks);
@@ -36,12 +37,26 @@ public class Demo2 extends DemoBase {
 		Melody chimem = m(
 				an(60), an(60), an(60), an(60));
 		Figure chimef = f(chimer, chimem, newage);
+		
+		Rhythm lead1r = r(
+				s(0.000,5.932,72), s(0.045,6.100,105), s(7,0.981,91), s(8,2.008,100),
+				s(10,0.891,91), s(10.984,1.809,100), s(13,2.693,83), s(15.994,7.290,91),
+				s(16.032,7.467,96), s(24,1.888,78), s(26,0.806,83), s(27,1.856,78),
+				s(29,2.813,74), s(32.034,6.295,78), s(32.107,6.375,66), s(39,0.923,83),
+				s(40.064,2.037,91), s(42,0.726,74), s(43.066,1.946,74), s(45,2.909,105),
+				s(48.060,6.329,100), s(55,1.067,58), s(56,7.867,96));
+		Melody lead1m = m(
+			an(72), an(65), an(65), an(67), an(68), an(67), an(65), an(72),
+			an(65), an(72), an(70), an(68), an(70), an(65), an(72), an(65),
+			an(67), an(68), an(70), an(68), an(65), an(63), an(65));
+		Figure lead1f = f(lead1r, lead1m, lead);
 
 		// This is the basic percussion and bass line
 		add1(kicksf);
 		add1(kicksf);
 		add1(kicksf, basichihatf);
 		add1(kicksf, basichihatf);
+		v(bass, 0.6); // quiet the bass just a bit
 		rpt(24, () ->
 			add1(kicksf, basichihatf, bassf));
 		
@@ -50,15 +65,14 @@ public class Demo2 extends DemoBase {
 		at(16, chimef);
 		
 		// Turn down the volume a bit on the chime-y part
-		v(newage, 0.3);
+		v(newage, 0.4);
 		
-		// Turn the volume up as the chime-y part repeats
-		v(16, newage, 0.4);
-		v(17, newage, 0.5);
-		v(18, newage, 0.6);
-		v(19, newage, 0.7);
+		// Lead part
+		at(16, lead1f);
+		v(lead, 0.3);
 		
 //		audition(lead);
+//		v(lead, 0.5);
 	}
 	
 	public static void main(String[] args) throws MidiUnavailableException, IOException {
