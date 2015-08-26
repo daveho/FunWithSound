f9c38b156bccc21814ae818a0ab368275c12711a
David Hovemeyer <david.hovemeyer@gmail.com>
2015-08-26 18:47:02 -0400

    draw piano keyboard
    this is in preparation for allowing the (computer) keyboard
    to be used for auditioning (live playing)

--- FunWithSoundProcessingLib/src/io/github/daveho/funwithsound/FunWithSound.java
+++ FunWithSoundProcessingLib/src/io/github/daveho/funwithsound/FunWithSound.java
@@ -33,7 +33,9 @@ public class FunWithSound {
 		player = new Player();
 		playing = false;
 		
+		parent.registerMethod("setup", this);
 		parent.registerMethod("dispose", this);
+		parent.registerMethod("draw", this);
 		
 		System.out.println("Starting ##library.name## version ##library.prettyVersion##");
 	}
@@ -42,6 +44,65 @@ public class FunWithSound {
 		player.stopPlaying();
 	}
 	
+	public void setup() {
+		System.out.println("setup called?");
+	}
+	
+	public void draw() {
+		// Draw piano keyboard
+		drawKeyboard();
+	}
+	
+	private static final int Y = 10;
+	private static final int X_INIT = 10;
+	private static final int W = 11;
+	private static final int H = 60;
+	
+	private void drawKeyboard() {
+		int cyc;
+		int x;
+		
+		parent.stroke(0);
+		parent.strokeWeight(1.0f);
+		
+		// Draw the white keys first
+		cyc = 9;  // cyc%12==0 means a C, note 21 is an A (9 half steps above C)
+		x = X_INIT;
+		for (int note = 21; note <= 108; note++) {
+			switch (cyc%12) {
+			case 1: case 3: case 6: case 8: case 10:
+				break;
+			default:
+				parent.fill(255);
+				parent.rect(x, Y, W, H);
+				x += W;
+				break;
+			}
+			
+			cyc++;
+		}
+		
+		// Then draw the black keys
+		cyc = 9;
+		x = X_INIT;
+		for (int note = 21; note <= 108; note++) {
+			switch (cyc%12) {
+			case 1: case 3: case 6: case 8: case 10:
+				// Black key
+				parent.fill(0);
+				parent.rect(x-((W/2)-2)-1, Y, W-2, (H*3)/5);
+				break;
+			default:
+				// White key
+				x += W;
+				break;
+			}
+			
+			cyc++;
+
+		}
+	}
+
 	public void play(Composer c) {
 		player.setComposition(c.getComposition());
 		player.startPlaying();
