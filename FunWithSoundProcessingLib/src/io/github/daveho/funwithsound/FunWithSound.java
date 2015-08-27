57d3e3c9c866989a088a20ee9b251123c14bbae6
David Hovemeyer <david.hovemeyer@gmail.com>
2015-08-26 21:49:01 -0400

    working on key event handling - not quite right yet

--- FunWithSoundProcessingLib/src/io/github/daveho/funwithsound/FunWithSound.java
+++ FunWithSoundProcessingLib/src/io/github/daveho/funwithsound/FunWithSound.java
@@ -15,8 +15,12 @@
 
 package io.github.daveho.funwithsound;
 
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
 
 import processing.core.*;
+import processing.event.KeyEvent;
 
 /**
  * FunWithSound Processing library.
@@ -27,15 +31,21 @@ public class FunWithSound {
 	PApplet parent;
 	Player player;
 	boolean playing;
+	int startNote; // which note corresponds to the leftmost keyboard key
+	BitSet noteOn;
 	
 	public FunWithSound(PApplet parent) {
 		this.parent = parent;
 		player = new Player();
 		playing = false;
+		startNote = 60;  // is always a C
+		noteOn = new BitSet();
 		
-		parent.registerMethod("setup", this);
 		parent.registerMethod("dispose", this);
 		parent.registerMethod("draw", this);
+		System.out.println("Hey ya");
+		parent.registerMethod("keyEvent", this);
+		System.out.println("Smooooooove");
 		
 		System.out.println("Starting ##library.name## version ##library.prettyVersion##");
 	}
@@ -44,13 +54,60 @@ public class FunWithSound {
 		player.stopPlaying();
 	}
 	
-	public void setup() {
-		System.out.println("setup called?");
-	}
-	
 	public void draw() {
 		// Draw piano keyboard
 		drawKeyboard();
+	}
+
+	// Map of key codes to offsets from current start note.
+	//  e r   y u i    are the black keys
+	// s d f g h j k l are the white keys
+	private static final Map<Integer, Integer> NOTE_MAP = new HashMap<Integer,Integer>();
+	static {
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_S, 0);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_E, 1);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_D, 2);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_R, 3);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_F, 4);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_G, 5);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_Y, 6);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_H, 7);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_U, 8);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_J, 9);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_I, 10);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_K, 11);
+		NOTE_MAP.put(java.awt.event.KeyEvent.VK_L, 12);
+	}
+	
+	public void keyEvent(KeyEvent e) {
+		//System.out.println("key event!!!");
+		
+		int action = e.getAction();
+		if (action == KeyEvent.PRESS) {
+			System.out.println("Press");
+		} else if (action == KeyEvent.RELEASE) {
+			System.out.println("Release");
+		}
+/*
+		java.awt.event.KeyEvent e = (java.awt.event.KeyEvent) e_.getNative();
+		
+		boolean isPress = (e.getModifiers() & java.awt.event.KeyEvent.KEY_PRESSED) != 0;
+		boolean isRelease = (e.getModifiers() & java.awt.event.KeyEvent.KEY_RELEASED) != 0;
+		if (!(isPress || isRelease)) {
+			return;
+		}
+
+		System.out.println("press or release");
+		
+		if (NOTE_MAP.containsKey(e.getKeyCode())) {
+			int offset = NOTE_MAP.get(e.getKeyCode());
+			if (isPress) {
+				noteOn.set(startNote + offset);
+			} else {
+				noteOn.clear(startNote + offset);
+			}
+		}
+*/
 	}
 	
 	private static final int Y = 10;
@@ -71,9 +128,18 @@ public class FunWithSound {
 		for (int note = 21; note <= 108; note++) {
 			switch (cyc%12) {
 			case 1: case 3: case 6: case 8: case 10:
+				// Black key
 				break;
 			default:
-				parent.fill(255);
+				// White key
+				if (noteOn.get(note)) {
+					parent.fill(0,0,255);
+				} else if (note >= startNote && note <= startNote+12) {
+					// This note is in the active octave
+					parent.fill(152,251,152);
+				} else {
+					parent.fill(255);
+				}
 				parent.rect(x, Y, W, H);
 				x += W;
 				break;
@@ -89,7 +155,11 @@ public class FunWithSound {
 			switch (cyc%12) {
 			case 1: case 3: case 6: case 8: case 10:
 				// Black key
-				parent.fill(0);
+				if (noteOn.get(note)) {
+					parent.fill(0,0,255);
+				} else {
+					parent.fill(0);
+				}
 				parent.rect(x-((W/2)-2)-1, Y, W-2, (H*3)/5);
 				break;
 			default:
@@ -99,7 +169,6 @@ public class FunWithSound {
 			}
 			
 			cyc++;
-
 		}
 	}
 
