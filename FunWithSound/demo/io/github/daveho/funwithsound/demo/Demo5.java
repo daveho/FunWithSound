600f12581519c56fe10c54c905f548523b3523b3
David Hovemeyer <david.hovemeyer@gmail.com>
2015-08-30 09:10:17 -0400

    improved the beat

--- FunWithSound/demo/io/github/daveho/funwithsound/demo/Demo5.java
+++ FunWithSound/demo/io/github/daveho/funwithsound/demo/Demo5.java
@@ -21,8 +21,11 @@ public class Demo5 extends DemoBase {
 		sp.addSample(1, SPDIR + "/torvalds/torvalds-says-linux.wav", 3162, 3600);
 		v(sp, 0.4);
 
-		Instrument drumkit = percussion(TR808);
+		//Instrument drumkit = percussion(TR808);
 		
+		Instrument tr909 = percussion(TR909);
+
+		/*
 		Rhythm chihatr = rr(p(0, 101), .5, 4);
 		Rhythm hihatr = gr(chihatr, r(s(2, 1.2, 101)), sr(4, chihatr), r(s(6, 1.2, 101)));
 		Melody hihatm = m(an(42),an(42),an(42),an(42),an(46),an(42),an(42),an(42),an(42),an(46));
@@ -31,6 +34,17 @@ public class Demo5 extends DemoBase {
 		Rhythm kicksr = r(p(0, 101), p(3.5, 88), p(4, 101));
 		Melody kicksm = m(an(36), an(63), an(36));
 		Figure kicksf = f(kicksr, kicksm, drumkit);
+		*/
+//		Rhythm hihatr = rr(p(0, 101), 1, 8);
+//		Figure hihatf = pf(hihatr, 42, drumkit);
+//		Rhythm kicksr = rr(p(0, 101), 4, 2);
+//		Figure kicksf = pf(kicksr, 36, drumkit);
+		
+		Rhythm percr = r(
+				s(0.000,0.394,110), s(1,0.391,118), s(2,0.638,118), s(4,0.399,118), s(5,0.389,110), s(6,0.477,118));
+		Melody percm = m(
+				an(36), an(36), an(40), an(36), an(36), an(40));
+		Figure percf = f(percr, percm, tr909);
 		
 		Rhythm ltr = r(p(0));
 		Melody ltm = m(an(0));
@@ -42,11 +56,15 @@ public class Demo5 extends DemoBase {
 
 		int where = m();
 		
-		add1n(8, gf(hihatf, kicksf));
+		add1n(10, percf);
 		at(where+2, ltf);
 		at(where+4, llf);
 		at(where+5, llf);
 		at(where+6, ltf);
+		at(where+8, llf);
+		at(where+9, llf);
+		
+//		audition(accent);
 	}
 	
 	public static void main(String[] args) throws MidiUnavailableException, IOException {
