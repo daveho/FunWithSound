204dd2a60edd6f068465744eeef23be3214ad977
David Hovemeyer <david.hovemeyer@gmail.com>
2015-08-31 10:16:37 -0400

    major cleanup of LinuxTechno
    organized entire composition measure by measure
    
    tweaked the bleep parts
    
    added a second bass figure

--- FunWithSound/demo/io/github/daveho/funwithsound/demo/LinuxTechno.java
+++ FunWithSound/demo/io/github/daveho/funwithsound/demo/LinuxTechno.java
@@ -57,8 +57,11 @@ public class LinuxTechno extends DemoBase {
 		v(bassint, .8);
 		
 		//Instrument bass = instr(TB303, 9); // best: 5,9 good: 2,4,7,8  ok: 3 weird: 11,12,16(fun for break)
-		Instrument bass = instr(ARACHNO, 36);
+		Instrument bass = instr(ARACHNO, 39);
 		v(bass, 0.5);
+		
+		Instrument bass2 = instr(ARACHNO, 39);
+		//v(bass2, 0.6);
 		
 		Instrument bleep = instr(TB303, 16);
 		v(bleep, 0.7);
@@ -85,6 +88,13 @@ public class LinuxTechno extends DemoBase {
 				an(38), an(38), an(38), an(41), an(43), an(38), an(50));
 		Figure bass1f = f(bass1r, xm(-1,bass1m), bass);
 		
+		Rhythm bass2r= r(
+				s(0.000,0.476,102), s(1,0.447,118), s(2,0.434,118), s(2.5,0.665,118),
+				s(3.5,0.663,118), s(5,1.764,127), s(7,.6,115));
+		Melody bass2m = m(
+				an(38), an(38), an(38), an(40), an(36), an(36), an(33));
+		Figure bass2f = f(bass2r, bass2m, bass);
+		
 		Rhythm ltr = r(p(0));
 		Melody ltm = m(an(0));
 		Figure ltf = f(ltr, ltm, sp);
@@ -94,74 +104,71 @@ public class LinuxTechno extends DemoBase {
 		Figure llf = f(llr, llm, sp);
 		
 		Rhythm decayr = r(p(0, 127), p(2, 127), p(4, 110), p(6, 110), p(8, 100), p(10, 90), p(12, 80));
-		Melody decaym = m(an(61),an(61),an(78),an(60),an(74),an(75),an(75));
+		Melody decaym = m(an(61),an(61),an(75),an(60),an(74),an(60),an(75));
 		Figure decayf = f(decayr, decaym, conga);
 		
 		Rhythm bleep1r = r(
 				s(0.000,1.795,118), s(2,0.9,110),
-				s(3,0.416,110), s(3.400,1.523,102), s(5,0.940,118)/*, s(6,1.205,106), s(7,0.879,110)*/);
+				s(3,0.416,110), s(3.400,1.523,102), s(5,0.940,118));
 		Melody bleep1m = m(
 				an(62), an(67),
-				an(67), an(71), an(69)/*, an(72), an(71)*/);
+				an(67), an(71), an(69));
 		Figure bleep1f = f(bleep1r, bleep1m, bleep);
 		
 		Rhythm bleep2r = r(
-				s(0.000,0.870,106), s(0.925,3.978,99), s(4.950,3.821,99), s(8.975,4.008,96), s(8.987,3.992,106),
-				s(15.864,1.023,110), s(16.953,4.064,110), s(20.977,4.151,106), s(25.111,3.962,96), s(25.128,3.932,106));
+				s(0.000,0.870,106), s(0.925,3.978,99), s(4.950,3.821,99), s(8.987,3.992,106),
+				s(15.864,1.023,110), s(16.953,4.064,110), s(20.977,4.151,106), s(25.128,3.932,106));
 		Melody bleep2m = m(
-				an(62), an(72), an(71), an(64), an(69),
-				an(62), an(71), an(69), an(60), an(67));
+				an(62), an(72), an(71), an(69),
+				an(62), an(71), an(69), an(67));
+		Melody bleep3m = m(
+				an(62), an(72), an(71), an(64),
+				an(62), an(71), an(69), an(60));
 		Figure bleep2f = f(sr(-1,bleep2r), bleep2m, bleep2);
+		Figure bleep3f = f(sr(-1,bleep2r), bleep3m, bleep2);
+		
+/* 0*/	add1(gf(percf));
+/* 1*/	add1(gf(percf));
+/* 2*/	add1(gf(percf,bassintf,ltf));
+/* 3*/	add1(gf(percf));
+/* 4*/	add1(gf(percf, llf));
+/* 5*/	add1(gf(percf, llf));
+/* 6*/	add1(gf(percf,ltf));
+/* 7*/	add1(gf(percf));
+/* 8*/	add1(gf(percf,llf));
+/* 9*/	add1(gf(percf,llf));
+/*10*/	add1(gf(decayf));
+/*11*/	add1(gf());
+/*12*/	add1(gf(percf,bass1f));
+/*13*/	add1(gf(percf,bass1f));
+/*14*/	add1(gf(percf,bass1f,llf));
+/*15*/	add1(gf(percf,bass2f,llf));
+/*16*/	add1(gf(percf,bass1f,bassintf));
+/*17*/	add1(gf(percf,bass1f));
+/*18*/	add1(gf(percf,bass1f,llf));
+/*19*/	add1(gf(percf,bass2f,llf));
+/*20*/	add1(gf(percf,bass1f));
+/*21*/	add1(gf(percf,bass1f));
+/*22*/	add1(gf(percf,bass1f,llf));
+/*23*/	add1(gf(percf,bass2f,llf));
+/*24*/	add1(gf(percf,bass1f,bleep1f));
+/*25*/	add1(gf(percf,bass1f,bleep2f));
+/*26*/	add1(gf(percf,bass1f,bleep1f));
+/*27*/	add1(gf(percf,bass2f));
+/*28*/	add1(gf(percf,bass1f,bleep1f));
+/*29*/	add1(gf(percf,bass1f,bleep3f));
+/*30*/	add1(gf(percf,bass1f,bleep1f));
+/*31*/	add1(gf(percf,bass2f));
+/*32*/	add1(gf(percf,bass1f,llf));
+/*33*/	add1(gf(percf,bass1f,llf));
+/*34*/	add1(gf(percf,bass1f,llf));
+/*35*/	add1(gf(percf,bass2f));
+/*36*/	add1(gf(percf,bass1f,llf,bassintf));
+/*37*/	add1(gf(percf,bass1f,llf));
+/*38*/	add1(gf(percf,bass1f,llf));
+/*39*/	add1(gf(percf,bass2f));
 
-		int where = m();
-		// /*
-		add1n(10, percf);
-		at(where+2, bassintf);
-		at(where+2, ltf);
-		at(where+4, llf);
-		at(where+5, llf);
-		at(where+6, ltf);
-		at(where+8, llf);
-		at(where+9, llf);
-		
-		at(where+10, decayf);
-		// */
-		int off = 12/*0*/;
-		// /*
-		at(where+off+2, llf);
-		at(where+off+3, llf);
-		at(where+off+4, bassintf);
-		//at(where+off+5, llf);
-		//at(where+off+6, llf);
-		at(where+off+6, llf);
-		at(where+off+7, llf);
-		//at(where+off+9, llf);
-		//at(where+off+10, llf);
-		at(where+off+10, llf);
-		at(where+off+11, llf);
-		// */
-		for (int i = 0; i < 32; i++) {
-			at(where+off+i, gf(percf,bass1f));
-		}
-		
-		off += 12;
-		for (int i = 0; i <= 9; i++ ){
-			if (i%2==0) {
-				at(where+off+i, bleep1f);
-			}
-			if (i%4 == 1) {
-				at(where+off+i, bleep2f);
-			}
-		}
-		
-		off += 12;
-		at(where+off+0, llf);
-		at(where+off+1, llf);
-		at(where+off+2, llf);
-		at(where+off+3, llf);
-		at(where+off+4, bassintf);
-
-		audition(bleep2);
+		audition(bass2);
 	}
 	
 	public static void main(String[] args) throws MidiUnavailableException, IOException {
