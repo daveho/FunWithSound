a09b36de0029df0d5b4be2012f0b7175951ac4b1
David Hovemeyer <david.hovemeyer@gmail.com>
2015-09-11 11:45:38 -0400

    refactor InstrumentInfo to make it possible to create new instr types

--- FunWithSound/src/io/github/daveho/funwithsound/InstrumentInfo.java
+++ FunWithSound/src/io/github/daveho/funwithsound/InstrumentInfo.java
@@ -16,6 +16,7 @@
 package io.github.daveho.funwithsound;
 
 import io.github.daveho.gervill4beads.GervillUGen;
+import io.github.daveho.gervill4beads.Midi;
 import io.github.daveho.gervill4beads.ReceivedMidiMessageSource;
 
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ import java.util.List;
 import javax.sound.midi.Receiver;
 
 import net.beadsproject.beads.core.AudioContext;
+import net.beadsproject.beads.core.Bead;
 import net.beadsproject.beads.core.UGen;
 import net.beadsproject.beads.ugens.Gain;
 
@@ -31,21 +33,21 @@ import net.beadsproject.beads.ugens.Gain;
  * The synthesizer UGen and effects chain UGens for
  * a specific {@link Instrument}.
  */
-class InstrumentInfo {
+public class InstrumentInfo {
 	/** MidiMessages to be played by this instrument should be sent here. */
-	Receiver source;
+	public Receiver source;
 	
 	/**
 	 * UGen at the head of the effects chain. MidiMessages sent to the
 	 * source Receiver should be delivered (somehow) to this UGen.
 	 */
-	UGen head;
+	public UGen head;
 	
 	/** Reference to the UGen currently at the end of the effects chain. */ 
-	UGen tail;
+	public UGen tail;
 
 	/** Gain UGen (which is fed by the UGen at the end of the effects chain.) */
-	Gain gain;
+	public Gain gain;
 	
 	List<GainEvent> gainEvents;
 	
@@ -55,28 +57,42 @@ class InstrumentInfo {
 	 * @param gervill the GervillUGen
 	 */
 	public InstrumentInfo(GervillUGen gervill) {
-		this.source = gervill.getSynthRecv();
-		this.head = gervill;
-		this.gainEvents = new ArrayList<GainEvent>();
+		this(gervill.getSynthRecv(), gervill);
+	}
+	
+	/**
+	 * "Generic" constructor for instruments that wish to have MIDI
+	 * messages sequenced/delivered automatically by a
+	 * ReceivedMidiMessageSource.  Most custom instruments will
+	 * want to use this constructor.  The head UGen should
+	 * override the <code>messageReceived</code> method
+	 * and use the {@link Midi#hasMidiMessage(net.beadsproject.beads.core.Bead)}
+	 * and {@link Midi#getMidiMessage(Bead)} methods to detect and
+	 * process received MidiMessages.
+	 * 
+	 * @param head the UGen at the head of the effects chain
+	 *             (i.e., the synthesizer)
+	 * @param ac   the AudioContext
+	 */
+	public InstrumentInfo(UGen head, AudioContext ac) {
+		this(new ReceivedMidiMessageSource(ac), head);
 		
-		// Initially, the GervillUGen is at the end of the effects
-		// chain (since there are no effects yet.)
-		this.tail = gervill;
+		// Deliver MidiMessages to the head UGen
+		((ReceivedMidiMessageSource)source).addMessageListener(head);
 	}
 
 	/**
-	 * Create an InstrumentInfo for a @{link SampleBankUGen}.
+	 * "Generic" constructor: this is appropriate for custom instruments
+	 * that define their own MIDI Receivers.
 	 * 
-	 * @param sb the SampleBankUGen
-	 * @param ac the AudioContext
+	 * @param source the MIDI Receiver
+	 * @param head   the UGen at the head of the effects chain
+	 *               (i.e., the synthesizer or audio source)
 	 */
-	public InstrumentInfo(SampleBankUGen sb, AudioContext ac) {
-		this.source = new ReceivedMidiMessageSource(ac);
-		this.head = sb;
+	public InstrumentInfo(Receiver source, UGen head) {
+		this.source = source;
+		this.head = head;
+		this.tail = head;
 		this.gainEvents = new ArrayList<GainEvent>();
-		this.tail = sb;
-		
-		// Deliver MidiMessages to the SampleBankUGen
-		((ReceivedMidiMessageSource)source).addMessageListener(head);
 	}
 }
